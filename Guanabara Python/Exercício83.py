5bfc37018db563921bdf34ad6391f467bcb91707
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-16 19:20:29 -0300

    mensagem

--- Guanabara Python/Exercício83.py
+++ Guanabara Python/Exercício83.py
@@ -1,10 +1,18 @@
 #Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 #Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
-'''Jeito errado na tentativa de ex: )a+b('''
-x = input('Digite a expressão: ')
-y = x.count("(")                            
-z = x.count(")")                            
-if y != z:
-    print('A expressão está errada!')
+x = str(input('Digite a expressão: '))
+y = []
+for a in x:
+    if a == '(':
+        y.append('(')
+    elif a == ')':
+        if len(y) > 0:
+            y.pop()
+        else:
+            y.append(')')
+            break
+if len(y) == 0:
+    print('Sua expressão está válida!')
 else:
-    print('A expressão está válida!')
+    print('Sua expressão está errada!')
+#fandangos
