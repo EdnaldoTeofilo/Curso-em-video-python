6fee66efd46e2ccf323fdf400986ca85affa7a40
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:51:06 -0300

    Sorteando um item na lista

--- Mundo_1_Fundamentos/ex019.py
+++ Mundo_1_Fundamentos/ex019.py
@@ -1,5 +1,5 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
-from random import choice
+from random import choice 
 a = input('Digite o nome do primeiro aluno:')
 b = input('Digite o nome do segundo aluno:')
 c = input('Digite o nome do terceiro aluno:')
