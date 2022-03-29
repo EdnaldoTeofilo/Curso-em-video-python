67cd9f89c629886895e11d082831af6b12aeda27
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:53:08 -0300

    Correção ortográfica e exclusão de comentários desnecessários

--- Mundo_1_Fundamentos/ex023.py
+++ Mundo_1_Fundamentos/ex023.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 #Ex: Digite um número: 1834 //// unidade:4 dezena:3 centena:8 milhar:1
 x = int(input('Digite um número: '))
-u = x // 1 % 10
+u = x // 1 % 10 
 d = x // 10 % 10
 c = x // 100 % 10
 m = x // 1000 % 10
