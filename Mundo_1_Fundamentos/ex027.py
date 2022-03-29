b4f56088df76b84b17363348b0b9601ad43f8c94
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:55:45 -0300

    Primeiro e último nome de uma pessoa

--- Mundo_1_Fundamentos/ex027.py
+++ Mundo_1_Fundamentos/ex027.py
@@ -1,5 +1,5 @@
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 x = str(input('Digite seu nome completo: ')).strip()
-y = (x.split())
+y = (x.split()) 
 print('Seu primeiro nome é: {}'.format(y[0]))
 print('Seu último nome é: {}'.format(y[len(y)-1]))
