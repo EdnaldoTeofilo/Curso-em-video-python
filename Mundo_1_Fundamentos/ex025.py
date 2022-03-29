c6c4eb22b180b916ad17ce1688c605e91ce22169
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:54:34 -0300

    Procurando uma string dentro de outra

--- Mundo_1_Fundamentos/ex025.py
+++ Mundo_1_Fundamentos/ex025.py
@@ -1,3 +1,3 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
-nome = str(input('Qual é seu nome completo? ')).strip()
+nome = str(input('Qual é seu nome completo? ')).strip() 
 print ('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
