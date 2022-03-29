89b18ce73a6dd0cc8b876a17a289b09e4a3f086d
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:02:17 -0300

    Antecessor e Sucessor

--- Mundo_1_Fundamentos/ex005.py
+++ Mundo_1_Fundamentos/ex005.py
@@ -1,3 +1,3 @@
-#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+#Faça um  programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 x = int(input('Digite um número: '))
 print(f'Analisando o valor {x}, seu antecessor é {x-1} e o seu sucessor é {x+1}.')
