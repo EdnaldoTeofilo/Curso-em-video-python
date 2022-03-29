0afd01f445d384415bb7222d5ed5e570caf6fc06
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:08:42 -0300

    Índice de Massa Corporal (IMC)

--- Mundo_2_EstruturasDeControle/ex043.py
+++ Mundo_2_EstruturasDeControle/ex043.py
@@ -1,5 +1,5 @@
 #Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
-#-Abaixo de 18.5: Abaixo do Peso   -Entre 18.5 e 25: Peso ideal   - 25 até 30: Sobrepeso      - 30 até 40: obesidade     - Acima de 40: Obesidade mórbida
+#-Abaixo de 18.5: Abaixo do Peso   -Entre 18.5 e 25: Peso ideal   - 25 até 30: Sobrepeso      - 30 até 40: obesidade     - Acima de 40: Obesidade mórbida 
 
 p = float(input('Digite o seu peso em kilogramas: '))
 a = float(input('Digite sua altura em metros:'))
