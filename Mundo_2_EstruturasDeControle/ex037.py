edd6d9985d78238a51c0327040859bf0fe74e1c2
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 16:40:23 -0300

    Corrigir nomenclatura de conversão

--- Mundo_2_EstruturasDeControle/ex037.py
+++ Mundo_2_EstruturasDeControle/ex037.py
@@ -1,5 +1,5 @@
 #Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
-#1 para binário, 2 para octal, 3 para hexadecimal
+#1 para binário, 2 para octal, 3 para hexadecimal 
 
 x = int(input('Digite um número: '))
 y = int(input('Digite 1 para binário, 2 para octal, 3 para hexadecimal:'))
