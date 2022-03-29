1d6f5b87396e80b73d9a348784652ecee5ffabb7
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:59:03 -0300

    Ano Bissexto

--- Mundo_1_Fundamentos/ex032.py
+++ Mundo_1_Fundamentos/ex032.py
@@ -1,5 +1,5 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto. Ocorrendo a cada quatro anos (exceto anos múltiplos de 100 que não são múltiplos de 400).
-x= int(input('Digite um ano: '))
+x= int(input('Digite um ano: ')) 
 if x % 4 == 0 and x % 100 !=0 or x % 400 ==0:
     print ('O ano {} é um ano bissexto'.format(x))
 else:
