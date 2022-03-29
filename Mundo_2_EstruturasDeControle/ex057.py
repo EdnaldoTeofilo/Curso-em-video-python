bbde5a3655249789790133921647eee068d5fea5
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:16:03 -0300

    Validação de dados

--- Mundo_2_EstruturasDeControle/ex057.py
+++ Mundo_2_EstruturasDeControle/ex057.py
@@ -1,4 +1,4 @@
 #Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
-x = ''
+x = '' 
 while x != 'M' and x != 'F':
     x = str(input('Qual seu sexo [M/F]:')).strip().upper()
