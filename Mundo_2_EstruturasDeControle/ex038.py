524536d3fc85ce4af2b9be179f6f738cef0caf11
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 16:40:49 -0300

    Comparando números

--- Mundo_2_EstruturasDeControle/ex038.py
+++ Mundo_2_EstruturasDeControle/ex038.py
@@ -1,5 +1,5 @@
 #Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
-#O primeiro valor é maior, o segundo valor é maior, não existe valor maior, os dois são iguais
+#O primeiro valor é maior, o segundo valor é maior, não existe valor maior, os dois são iguais 
 x = int(input('Digite o primeiro número: '))
 y = int(input('Digite o segundo número: '))
 
