f730eb747b32d3d95754194a536376b6584ae8b8
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:09:06 -0300

    Gerenciador de pagamentos

--- Mundo_2_EstruturasDeControle/ex044.py
+++ Mundo_2_EstruturasDeControle/ex044.py
@@ -1,5 +1,5 @@
 #Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
-# à vista dinheiro/cheque: 10% de desconto    -À vista no cartão:5% de desconto     -Em até 2x no cartão:Preço normal     -3x ou mais no cartão:20% de juros
+# à vista dinheiro/cheque: 10% de desconto    -À vista no cartão:5% de desconto      -Em até 2x no cartão:Preço normal     -3x ou mais no cartão:20% de juros
 
 x = float(input('Digite o valor do produto: R$'))
 print('1)à vista dinheiro/cheque: 10% de desconto')
