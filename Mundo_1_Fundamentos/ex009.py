d75aafb019b3871f0827b33a68594a744143e4e7
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:04:47 -0300

    Tabuada

--- Mundo_1_Fundamentos/ex009.py
+++ Mundo_1_Fundamentos/ex009.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 x = int (input('Digite um número para ver sua tabuada: '))
-print('='*12)
+print('='*12) 
 print('{} x {:2} = {}'.format(x,1,x*1))
 print('{} x {:2} = {}'.format(x,2,x*2))
 print('{} x {:2} = {}'.format(x,3,x*3))
