8eac97efa3630f1b9e755115362a945b750961aa
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:14:02 -0300

    Maior e menor da sequência

--- Mundo_2_EstruturasDeControle/ex055.py
+++ Mundo_2_EstruturasDeControle/ex055.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 y = 0
-z = 0
+z = 0 
 
 for c in range (1,6):
     x = float(input('Digite o peso da {}º pessoa:'.format(c)))
