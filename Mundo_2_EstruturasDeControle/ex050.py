5b8622bb680c96dd5df313a77ebd75828dcf5c32
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:11:45 -0300

    Soma dos pares

--- Mundo_2_EstruturasDeControle/ex050.py
+++ Mundo_2_EstruturasDeControle/ex050.py
@@ -1,4 +1,4 @@
-#Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+#Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o. 
 x = 0
 for c in range (0,6):
     y = int(input('Digite um número:'))
