341eefeb11861f8431532a3c8223b4cb7e279f6c
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:21:17 -0300

    Estatísticas em produtos

--- Mundo_2_EstruturasDeControle/ex070.py
+++ Mundo_2_EstruturasDeControle/ex070.py
@@ -1,7 +1,7 @@
 #Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar.
 #No final, mostre: A)Qual é o total gasto na compra. B)Quantos produtos custam mais de R$1000. C)Qual é o nome do produto mais barato.
 total = acima = a = 0
-barato = ''
+barato = '' 
 while True:
     x = str(input('Nome do produto: '))
     y = int(input('Digite o preço: '))
