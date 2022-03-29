1d8d4a4200b959f4c735c1115c5f2d47420a77f3
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:28:17 -0300

    Número por extenso

--- Mundo_3_EstruturasCompostas/ex072.py
+++ Mundo_3_EstruturasCompostas/ex072.py
@@ -1,4 +1,4 @@
-#Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+#Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. 
 #Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 x = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
