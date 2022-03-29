73a9bd9cd69f7d2b0e534e41fef3a85f4b281075
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:10:54 -0300

    Soma ímpares múltiplos de três

--- Mundo_2_EstruturasDeControle/ex048.py
+++ Mundo_2_EstruturasDeControle/ex048.py
@@ -1,4 +1,4 @@
-#Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+#Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500. 
 x = 0
 for c in range (1,501,2):
     
