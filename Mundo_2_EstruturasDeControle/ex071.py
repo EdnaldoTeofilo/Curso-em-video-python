04e4bb2286d11e0fd04bb57d939f8cb8c4ca6c1d
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:26:48 -0300

    Simulador de caixa eletrônico

--- Mundo_2_EstruturasDeControle/ex071.py
+++ Mundo_2_EstruturasDeControle/ex071.py
@@ -1,5 +1,5 @@
 #Crie um programa que simule o funcionamento de um caixa eletrônico.
-#No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+#No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. 
 #OBS:Considere que o caixa possui cédulas de R$50,R$20,R$10 e R$1.
 from time import sleep 
 while True:
