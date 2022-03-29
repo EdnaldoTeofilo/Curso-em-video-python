a66a81619ed0accd8930966a6051acbcfb885d41
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 17:07:35 -0300

    Classificando Atletas

--- Mundo_2_EstruturasDeControle/ex041.py
+++ Mundo_2_EstruturasDeControle/ex041.py
@@ -1,5 +1,5 @@
 #A confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
-#Até 9 anos:MIRIM, Até 14 anos:INFANTIL, Até 19 anos:JUNIOR, Até 25 anos:SÊNIOR, Acima Master
+#Até 9 anos:MIRIM, Até 14 anos:INFANTIL, Até 19 anos:JUNIOR, Até 25 anos:SÊNIOR, Acima Master 
 from datetime import date
 
 n = int(input('Digite seu ano de nascimento: '))
