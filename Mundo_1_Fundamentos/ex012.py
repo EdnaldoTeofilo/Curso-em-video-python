4048b297af6aba9ad2a87aa8827ffc7c4d139ddd
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-03-29 15:45:22 -0300

    Calculando Descontos

--- Mundo_1_Fundamentos/ex012.py
+++ Mundo_1_Fundamentos/ex012.py
@@ -1,4 +1,4 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
-preço = float(input('Coloque o valor do produto: '))
+preço = float(input('Coloque o valor do produto: ')) 
 novo = preço * 0.95
 print(f'O produto que custava R${preço}, na promoção com descontro de 5% vai custar R${novo:.2f}.')
